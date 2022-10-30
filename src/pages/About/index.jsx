cf74fc0fdaa730c6620f533a634641bdc1c2efe1
Bercea Eduardo <berceaeduardo1@gmail.com>
2022-10-30 17:45:23 +0100

    Fonctionnement page About

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,8 +1,58 @@
+import "./about.css";
+
+import AboutImg from "../../assets/AboutImg.jpg";
+import DropDown from "../../components/Dropdown";
+
 function About() {
   return (
-    <div>
-      <h1>J'aime</h1>
-    </div>
+    <main className="container">
+      <img src={AboutImg} alt="Montagnes" className="img_banniere" />
+      <div className="collapse_about">
+        <DropDown
+          name="Fiabilité"
+          description={
+            <p>
+              Les annonces postées sur Kasa garantissent une fiabilité totale.
+              Les photos sont conformes aux logements, et toutes les
+              informations sont régulièrement vérifiées par nos équipes.
+            </p>
+          }
+        />
+        <DropDown
+          name="Respect"
+          description={
+            <p>
+              La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+              comportement discriminatoire ou de perturbation du voisinage
+              entraînera une exclusion de notre plateforme.
+            </p>
+          }
+        />
+        <DropDown
+          name="Service"
+          description={
+            <p>
+              Nos équipes se tiennent à votre disposition pour vous fournir une
+              expérience parfaite. N'hésitez pas à nous contacter si vous avez
+              la moindre question.
+            </p>
+          }
+        />
+        <DropDown
+          name="Sécurité"
+          description={
+            <p>
+              La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+              pour les voyageurs, chaque logement correspond aux critères de
+              sécurité établis par nos services. En laissant une note aussi bien
+              à l'hôte qu'au locataire, cela permet à nos équipes de vérifier
+              que les standards sont bien respectés. Nous organisons également
+              des ateliers sur la sécurité domestique pour nos hôtes.
+            </p>
+          }
+        />
+      </div>
+    </main>
   );
 }
 
