794388fb6f7496288e08473fb5c7cb35c4b48082
Bercea Eduardo <berceaeduardo1@gmail.com>
2022-10-30 17:42:17 +0100

    Création du DropDown

---
+++ src/components/Dropdown/index.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./dropdown.css";
+import flecheHaut from "../../assets/Drop.svg";
+import flecheBas from "../../assets/DropDownBas.svg";
+
+function DropDown(droptype) {
+  const [isClose, setIsClose] = useState(true);
+
+  return isClose ? (
+    <div className="collapse-bloc">
+      <button className="collapse-btn" onClick={() => setIsClose(false)}>
+        {droptype.name}
+        <img className="flecheBas" src={flecheBas} alt="Fleche vers le bas" />
+      </button>
+    </div>
+  ) : (
+    <div className="collapse-bloc">
+      <button className="collapse-btn" onClick={() => setIsClose(true)}>
+        {droptype.name}
+        <img
+          className="flecheHaut"
+          src={flecheHaut}
+          alt="Fleche vers le haut"
+        />
+      </button>
+      <div className="collapse-desc">{droptype.description}</div>
+    </div>
+  );
+}
+
+export default DropDown;
